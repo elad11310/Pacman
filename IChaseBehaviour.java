0bd68abe4a2758ad212fa94444385cfe1a1d4336
elad11310 <elad11310@gmail.com>
2021-12-13 02:34:25 +0200

    More documentation

--- IChaseBehaviour.java
+++ IChaseBehaviour.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
 public interface IChaseBehaviour {
+
+
+    // this interface is used to describe the behaviour of each ghost
+    // each ghost implements the chase function which determines the manner of chase to this ghost
+    // in addition destination function is implemented to return the destination point.
     ArrayList<Point> chase(Point start, IGraph graph);
 
     Point destination();
